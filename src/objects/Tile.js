2b5c977cddcbd7e66defb05f3a3581a268e855d0
matchthree3 <leework0518@gmail.com>
2026-07-25 20:53:30 +0800

    Update Tile.js

--- src/objects/Tile.js
+++ src/objects/Tile.js
@@ -7,15 +7,26 @@ export default class Tile extends Phaser.GameObjects.Sprite {
 
         this.row = row;
         this.col = col;
+        this.colorType = texture.replace('tile_', ''); // 紀錄顏色標籤 (red, blue, 等)
 
         this.setInteractive();
-        this.on('pointerdown', this.onClick, this);
+        this.on('pointerdown', this.onPointerDown, this);
     }
 
-    onClick() {
-        this.setTint(0xaaaaaa);
-        this.scene.time.delayedCall(100, () => {
-            this.clearTint();
-        });
+    onPointerDown() {
+        // 觸發 Board 的選取機制
+        if (this.scene.board && !this.scene.board.isBusy) {
+            this.scene.board.selectTile(this);
+        }
+    }
+
+    setSelected(selected) {
+        if (selected) {
+            this.setAlpha(0.6);
+            this.setScale(0.9);
+        } else {
+            this.setAlpha(1);
+            this.setScale(1);
+        }
     }
 }
