d1aa7ebe137e4ec008f889e83b823b740c169cfb
matchthree3 <leework0518@gmail.com>
2026-07-25 20:20:21 +0800

    Update BootScene.js

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,1 +1,29 @@
+import Phaser from 'phaser';
 
+export default class BootScene extends Phaser.Scene {
+    constructor() {
+        super('BootScene');
+    }
+
+    preload() {
+        const colors = {
+            'red': 0xff4d4d,
+            'blue': 0x4d94ff,
+            'green': 0x4dff4d,
+            'yellow': 0xffff4d,
+            'purple': 0xff4dff
+        };
+
+        for (const [key, color] of Object.entries(colors)) {
+            const graphics = this.add.graphics();
+            graphics.fillStyle(color, 1);
+            graphics.fillRoundedRect(0, 0, 64, 64, 16);
+            graphics.generateTexture(`tile_${key}`, 64, 64);
+            graphics.destroy();
+        }
+    }
+
+    create() {
+        this.scene.start('GameScene');
+    }
+}
