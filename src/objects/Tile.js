fb44ff37521dd768750adc3b05affb3fb4bcd52c
matchthree3 <leework0518@gmail.com>
2026-07-25 21:08:58 +0800

    Update Tile.js

--- src/objects/Tile.js
+++ src/objects/Tile.js
@@ -7,16 +7,50 @@ export default class Tile extends Phaser.GameObjects.Sprite {
 
         this.row = row;
         this.col = col;
-        this.colorType = texture.replace('tile_', ''); // 紀錄顏色標籤 (red, blue, 等)
+        this.colorType = texture.replace('tile_', '');
 
         this.setInteractive();
+        
+        this.scene.input.setDraggable(this);
+
         this.on('pointerdown', this.onPointerDown, this);
+        this.on('dragstart', this.onDragStart, this);
+        this.on('dragend', this.onDragEnd, this);
     }
 
     onPointerDown() {
-        // 觸發 Board 的選取機制
         if (this.scene.board && !this.scene.board.isBusy) {
             this.scene.board.selectTile(this);
+        }
+    }
+
+    onDragStart(pointer) {
+        if (this.scene.board && !this.scene.board.isBusy) {
+            this.startX = pointer.x;
+            this.startY = pointer.y;
+        }
+    }
+
+    onDragEnd(pointer) {
+        if (!this.scene.board || this.scene.board.isBusy) return;
+
+        const deltaX = pointer.x - this.startX;
+        const deltaY = pointer.y - this.startY;
+        const threshold = 20;
+
+        let targetRow = this.row;
+        let targetCol = this.col;
+
+        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+            if (deltaX > threshold) targetCol += 1;
+            else if (deltaX < -threshold) targetCol -= 1;
+        } else {
+            if (deltaY > threshold) targetRow += 1;
+            else if (deltaY < -threshold) targetRow -= 1;
+        }
+
+        if (targetRow !== this.row || targetCol !== this.col) {
+            this.scene.board.handleDragSwap(this, targetRow, targetCol);
         }
     }
 
