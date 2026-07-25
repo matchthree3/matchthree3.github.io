2c6cd7804f30e45209565563406eef1d85902f11
李祁軒 <98266591+0xseanlee@users.noreply.github.com>
2026-07-25 20:31:18 +0800

    Update Tile.js

--- src/objects/Tile.js
+++ src/objects/Tile.js
@@ -1,1 +1,21 @@
+import Phaser from 'phaser';
 
+export default class Tile extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, row, col) {
+        super(scene, x, y, texture);
+        scene.add.existing(this);
+
+        this.row = row;
+        this.col = col;
+
+        this.setInteractive();
+        this.on('pointerdown', this.onClick, this);
+    }
+
+    onClick() {
+        this.setTint(0xaaaaaa);
+        this.scene.time.delayedCall(100, () => {
+            this.clearTint();
+        });
+    }
+}
