26b43ee757b9f6549c267e95f1083d627f28ca6e
matchthree3 <leework0518@gmail.com>
2026-07-25 20:19:43 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
+import Phaser from 'phaser';
+import BootScene from './scenes/BootScene.js';
+import GameScene from './scenes/GameScene.js';
 
+const config = {
+    type: Phaser.AUTO,
+    width: 540,
+    height: 960,
+    parent: 'game-container',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    backgroundColor: '#302c34',
+    scene: [BootScene, GameScene]
+};
+
+new Phaser.Game(config);
