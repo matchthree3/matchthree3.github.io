c862700c251a4c953a596df30e927381ee124709
matchthree3 <leework0518@gmail.com>
2026-07-25 20:20:49 +0800

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,1 +1,18 @@
+import Phaser from 'phaser';
+import Board from '../objects/Board.js';
 
+export default class GameScene extends Phaser.Scene {
+    constructor() {
+        super('GameScene');
+    }
+
+    create() {
+        this.add.text(270, 100, 'Score: 0', {
+            fontSize: '36px',
+            fontStyle: 'bold',
+            color: '#ffffff'
+        }).setOrigin(0.5);
+
+        this.board = new Board(this, 270, 520);
+    }
+}
