d6623a6f7fbc91cb0d29e933f011001939ac5fec
matchthree3 <leework0518@gmail.com>
2026-07-27 15:58:43 +0800

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -7,11 +7,16 @@ export default class GameScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(270, 100, 'Score: 0', {
-            fontSize: '36px',
+        // 使用高解析度字形渲染，大幅提升高清螢幕下的文字銳利度
+        const scoreText = this.add.text(270, 100, 'Score: 0', {
+            fontSize: '64px',
             fontStyle: 'bold',
-            color: '#ffffff'
+            color: '#ffffff',
+            align: 'center'
         }).setOrigin(0.5);
+
+        // 縮放回原本合適的大小
+        scoreText.setScale(0.6);
 
         this.board = new Board(this, 270, 520);
     }
