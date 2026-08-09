2e21b669e8304c477b3e02c794dcac8f9106f843
matchthree3 <leework0518@gmail.com>
2026-08-09 15:00:45 +0800

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -6,18 +6,22 @@ export default class GameScene extends Phaser.Scene {
         super('GameScene');
     }
 
-    create() {
-        // 使用高解析度字形渲染，大幅提升高清螢幕下的文字銳利度
-        const scoreText = this.add.text(270, 100, 'Score: 0', {
+    create(data) {
+        // 1. 初始化分數與關卡資料 (解決分數不增加的 Bug)
+        this.score = 0;
+        this.levelData = data || { id: 1, target: 1000, moves: 20 };
+
+        // 2. 顯示高解析度分數文字並掛載至場景
+        this.scoreText = this.add.text(270, 80, 'Score: 0', {
             fontSize: '64px',
             fontStyle: 'bold',
             color: '#ffffff',
             align: 'center'
         }).setOrigin(0.5);
 
-        // 縮放回原本合適的大小
-        scoreText.setScale(0.6);
+        this.scoreText.setScale(0.6);
 
+        // 3. 建立棋盤物件
         this.board = new Board(this, 270, 520);
     }
 }
